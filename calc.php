9ca9bcb0cd72921e3656f32fdbe6609ca8b2f564
bimla-it19 <bimla.it19@bitsathy.ac.in>
2019-10-03 21:10:21 +0530

    first commit

---
+++ calc.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+ </head>
+ <body>
+ <?php
+ $a=4;
+ $b=4;
+ echo "add:".$a+$b."<br>";
+ echo "sub:".$a-$b."<br>";
+ echo "mul:".$a*$b."<br>";
+ echo "div:".$a/$b."<br>";
+?>
+</body>
+</html>
